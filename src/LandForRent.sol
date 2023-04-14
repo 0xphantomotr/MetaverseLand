b4621954746446384186a7c597de3a5d75b839f3
Phantom Otr <mehmetigenci6.com>
2023-04-14 22:50:39 +0200

    save

--- src/LandForRent.sol
+++ src/LandForRent.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract LandForRent {
 
